e19a136b890636d7957bf3c89a283be3f5fe07bb
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-05 14:08:00 +0700

    Add logistic regression model training script

---
+++ MLProject/modelling.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import numpy as np
+import mlflow
+import mlflow.sklearn
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LogisticRegression
+
+if __name__ == "__main__":
+    mlflow.autolog()
+
+    df = pd.read_csv("namadataset_preprocessing/dataset_clean.csv")
+    X = df.drop(columns=['target'])
+    y = df['target']
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+    with mlflow.start_run():
+        model = LogisticRegression(max_iter=1000)
+        model.fit(X_train, y_train.values.ravel())
+
+        accuracy = model.score(X_test, y_test)
+        print(f"Retraining Berhasil! Akurasi Model Baru: {accuracy:.4f}")
