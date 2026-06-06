ac1d7bd79ea723006bad940b32f5721116cb0585
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 03:39:37 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,35 +1,19 @@
-import os
-import pandas as pd
-import numpy as np
 import mlflow
 import mlflow.sklearn
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+import pandas as pd
 from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
 
-if os.path.exists("heart_preprocesing.csv"):
-    DATA_PATH = "heart_preprocesing.csv"
-elif os.path.exists("MLProject/heart_preprocesing.csv"):
-    DATA_PATH = "MLProject/heart_preprocesing.csv"
-else:
-    DATA_PATH = "heart_preprocesing.csv"
+mlflow.sklearn.autolog()
 
-print(f"Membaca dataset dari: {DATA_PATH}")
-
-df = pd.read_csv(DATA_PATH)
+df = pd.read_csv('heart_preprocesing.csv')
 X = df.drop(columns=['target'])
 y = df['target']
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-scaler = StandardScaler()
-X_train = scaler.fit_transform(X_train)
-X_test = scaler.transform(X_test)
+mlflow.set_experiment("Heart_Disease")
 
-with mlflow.start_run():
+with mlflow.start_run(run_name="logistic_regression_basic"):
     model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train)
-    
-    accuracy = model.score(X_test, y_test)
-    print("=== TRAINING BERHASIL ===")
-    print(f"Akurasi Model: {accuracy:.4f}")
+    model.fit(X_train, y_train.values.ravel())
+    print("Training Selesai dengan Autolog lokal!")
