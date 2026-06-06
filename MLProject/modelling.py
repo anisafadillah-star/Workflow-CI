8635d1f0d95cba28be3bfeb8a3ffe34d6979b49c
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-06 14:53:33 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,13 +6,16 @@ import mlflow.sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score, classification_report
 
 if os.path.exists("heart_preprocesing.csv"):
     DATA_PATH = "heart_preprocesing.csv"
-elif os.path.exists("namadataset_preprocessing/heart_preprocesing.csv"):
-    DATA_PATH = "namadataset_preprocessing/heart_preprocesing.csv"
+elif os.path.exists("MLProject/heart_preprocesing.csv"):
+    DATA_PATH = "MLProject/heart_preprocesing.csv"
 else:
-    raise FileNotFoundError("Dataset heart_preprocesing.csv tidak ditemukan di folder mana pun!")
+    DATA_PATH = "Membangun_model/heart_preprocesing.csv"
+
+print(f"Membaca dataset tunggal dari: {DATA_PATH}")
 
 df = pd.read_csv(DATA_PATH)
 X = df.drop(columns=['target'])
@@ -28,5 +31,9 @@ with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
     model.fit(X_train, y_train.values.ravel())
 
-    accuracy = model.score(X_test, y_test.values.ravel())
-    print(f"Training Selesai Berhasil! Akurasi Model: {accuracy:.4f}")
+    y_pred = model.predict(X_test)
+    accuracy = accuracy_score(y_test, y_pred)
+    
+    print("=== TRAINING BERHASIL ===")
+    print("Accuracy:", accuracy)
+    print("\nClassification Report:\n", classification_report(y_test, y_pred))
