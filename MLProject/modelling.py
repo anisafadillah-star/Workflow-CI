6a46e1b5cee87ab6fe6a9b5b6d5eaee603b688a3
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-06 15:35:41 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -7,7 +7,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 
-# PENDETEKSI JALUR FILE OTOMATIS
 if os.path.exists("heart_preprocesing.csv"):
     DATA_PATH = "heart_preprocesing.csv"
 elif os.path.exists("MLProject/heart_preprocesing.csv"):
@@ -17,26 +16,20 @@ else:
 
 print(f"Membaca dataset dari: {DATA_PATH}")
 
-# 1. Membaca Dataset Berupa File Tunggal
 df = pd.read_csv(DATA_PATH)
 X = df.drop(columns=['target'])
 y = df['target']
 
-# 2. Membagi Data Training & Testing (Split 80:20)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-# 3. Standarisasi Fitur Menggunakan StandardScaler
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# 4. Menjalankan Training dengan Logging MLflow Secara Manual
 with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
-    # y_train langsung dimasukkan tanpa .values.ravel() agar tidak eror
     model.fit(X_train, y_train)
     
-    # Menghitung skor akurasi akhir
     accuracy = model.score(X_test, y_test)
     print("=== TRAINING BERHASIL ===")
     print(f"Akurasi Model: {accuracy:.4f}")
