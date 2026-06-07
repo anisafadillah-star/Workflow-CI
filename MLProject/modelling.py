54d60552989d1b462a4e50382ca13826832e3d0a
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 20:04:38 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -6,7 +6,7 @@ from sklearn.model_selection import train_test_split
 
 mlflow.sklearn.autolog()
 
-df = pd.read_csv('heart_preprocesing.csv')
+df = pd.read_csv('MLProject/heart_preprocesing.csv')
 X = df.drop(columns=['target'])
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
