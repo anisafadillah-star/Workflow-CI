a4e729606018b9ec68c94252535b3a1319765e3e
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 19:37:50 +0700

    Update prometheus_exporter.py

--- MLProject/prometheus_exporter.py
+++ MLProject/prometheus_exporter.py
@@ -1,26 +1,31 @@
-from prometheus_client import start_http_server, Counter, Gauge
+from prometheus_client import start_http_server
+from prometheus_client import Counter
+from prometheus_client import Gauge
+import psutil
 import time
 
 REQUEST_COUNT = Counter(
-    'prediction_requests_total',
-    'Total prediction requests'
-)
-
-MODEL_ACCURACY = Gauge(
-    'model_accuracy',
-    'Current model accuracy'
+    "prediction_requests_total",
+    "Total prediction requests"
 )
 
 CPU_USAGE = Gauge(
-    'cpu_usage_percent',
-    'CPU Usage'
+    "cpu_usage_percent",
+    "CPU Usage"
+)
+
+MEMORY_USAGE = Gauge(
+    "memory_usage_percent",
+    "Memory Usage"
 )
 
 start_http_server(8000)
 
 while True:
     REQUEST_COUNT.inc()
-    MODEL_ACCURACY.set(0.78)
-    CPU_USAGE.set(50)
+
+    CPU_USAGE.set(psutil.cpu_percent())
+
+    MEMORY_USAGE.set(psutil.virtual_memory().percent)
 
     time.sleep(5)
