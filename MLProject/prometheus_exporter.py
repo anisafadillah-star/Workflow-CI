8ae4cde9da2679f52d54a9972fec7150accb15a0
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 19:46:10 +0700

    Update prometheus_exporter.py

--- MLProject/prometheus_exporter.py
+++ MLProject/prometheus_exporter.py
@@ -1,31 +1,47 @@
-from prometheus_client import start_http_server
-from prometheus_client import Counter
-from prometheus_client import Gauge
+from prometheus_client import start_http_server, Counter, Gauge
 import psutil
 import time
 
+# Total request prediksi
+
 REQUEST_COUNT = Counter(
-    "prediction_requests_total",
-    "Total prediction requests"
+"prediction_requests_total",
+"Total prediction requests"
 )
+
+# Akurasi model
+
+MODEL_ACCURACY = Gauge(
+"model_accuracy",
+"Current model accuracy"
+)
+
+# CPU Usage
 
 CPU_USAGE = Gauge(
-    "cpu_usage_percent",
-    "CPU Usage"
+"cpu_usage_percent",
+"CPU Usage"
 )
 
+# Memory Usage
+
 MEMORY_USAGE = Gauge(
-    "memory_usage_percent",
-    "Memory Usage"
+"memory_usage_percent",
+"Memory Usage"
 )
 
 start_http_server(8000)
 
 while True:
-    REQUEST_COUNT.inc()
+REQUEST_COUNT.inc()
 
-    CPU_USAGE.set(psutil.cpu_percent())
+```
+# Isi dengan akurasi model yang kamu dapat saat training
+MODEL_ACCURACY.set(0.7869)
 
-    MEMORY_USAGE.set(psutil.virtual_memory().percent)
+CPU_USAGE.set(psutil.cpu_percent())
 
-    time.sleep(5)
+MEMORY_USAGE.set(psutil.virtual_memory().percent)
+
+time.sleep(5)
+```
