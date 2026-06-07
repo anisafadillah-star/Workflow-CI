ce3333031f9c3c5f18867f7abd22e34bb3039681
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 21:45:08 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,6 +1,7 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
@@ -8,11 +9,18 @@ from sklearn.linear_model import LogisticRegression
 mlflow.set_tracking_uri("file:./mlruns")
 mlflow.set_experiment("Heart_Disease")
 
-df = pd.read_csv('MLProject/heart_preprocesing.csv')
+df = pd.read_csv("MLProject/heart_preprocesing.csv")
 
-X = df.drop(columns=['target'])
-y = df['target']
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X = df.drop("target", axis=1)
+y = df["target"]
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X,
+    y,
+    test_size=0.2,
+    random_state=42
+)
+
 scaler = StandardScaler()
 
 X_train = scaler.fit_transform(X_train)
@@ -21,7 +29,11 @@ X_test = scaler.transform(X_test)
 mlflow.sklearn.autolog()
 
 with mlflow.start_run():
+
     model = LogisticRegression(max_iter=1000)
+
     model.fit(X_train, y_train)
+
     accuracy = model.score(X_test, y_test)
+
     print("Accuracy:", accuracy)
