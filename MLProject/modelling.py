cd1bd2286d2586da7763cf2a2309aae885ac13d6
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-07 21:35:11 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,19 +1,27 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
-mlflow.sklearn.autolog()
+mlflow.set_tracking_uri("file:./mlruns")
+mlflow.set_experiment("Heart_Disease")
 
 df = pd.read_csv('MLProject/heart_preprocesing.csv')
+
 X = df.drop(columns=['target'])
 y = df['target']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+scaler = StandardScaler()
 
-mlflow.set_experiment("Heart_Disease")
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-with mlflow.start_run(run_name="logistic_regression_basic"):
+mlflow.sklearn.autolog()
+
+with mlflow.start_run():
     model = LogisticRegression(max_iter=1000)
-    model.fit(X_train, y_train.values.ravel())
-    print("Training Selesai dengan Autolog lokal!")
+    model.fit(X_train, y_train)
+    accuracy = model.score(X_test, y_test)
+    print("Accuracy:", accuracy)
