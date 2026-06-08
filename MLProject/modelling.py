48dc9cfd8eab6c7e24e650bed0f817d8a7a7c7db
Anisa Fadillah Maulana <anisafadillahmaulana31@gmail.com>
2026-06-08 12:40:30 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,38 +1,47 @@
+import os
+import sys
+import warnings
+import pandas as pd
+import numpy as np
 import mlflow
 import mlflow.sklearn
-import pandas as pd
-import joblib
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.pipeline import Pipeline
 
-mlflow.set_tracking_uri("file:./mlruns")
-mlflow.set_experiment("Heart_Disease")
+if __name__ == "__main__":
+    warnings.filterwarnings("ignore")
+    np.random.seed(42)
 
-df = pd.read_csv("heart_preprocesing.csv")
+    file_path = sys.argv[3] if len(sys.argv) > 3 else os.path.join(os.path.dirname(os.path.abspath(__file__)), "heart_preprocesing.csv")
 
-X = df.drop("target", axis=1)
-y = df["target"]
+    if not os.path.exists(file_path):
+        print(f"Error: File {file_path} tidak ditemukan!")
+        sys.exit(1)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    X, y,
-    test_size=0.2,
-    random_state=42
-)
+    df = pd.read_csv(file_path)
 
-mlflow.sklearn.autolog()
+    X = df.drop(columns=['target'])
+    y = df['target']
 
-with mlflow.start_run():
-    full_pipeline = Pipeline([
-        ('scaler', StandardScaler()),
-        ('classifier', LogisticRegression(max_iter=1000))
-    ])
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    full_pipeline.fit(X_train, y_train)
+    mlflow.set_tracking_uri("file:./mlruns")
+    mlflow.set_experiment("Latihan Credit Scoring")
 
-    accuracy = full_pipeline.score(X_test, y_test)
-    print("Accuracy:", accuracy)
+    mlflow.sklearn.autolog(log_models=True)
 
-    joblib.dump(full_pipeline, "model.pkl")
-    print("Model pipeline sukses disimpan ke 'model.pkl'!")
+    with mlflow.start_run():
+
+        pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('classifier', LogisticRegression(max_iter=1000))
+        ])
+
+        pipeline.fit(X_train, y_train)
+
+        accuracy = pipeline.score(X_test, y_test)
+        print(f"Akurasi Model Berhasil Dicatat: {accuracy}")
+
+        mlflow.log_metric("accuracy", accuracy)
